ed97085ecb2240913719646a642c30c1e8e21eca
coberndorm <60147116+coberndorm@users.noreply.github.com>
2020-03-31 11:44:02 -0500

    Update Lectura Datos.py
    Correccion errores para mejorar complejidad

--- proyecto/codigo/Lectura Datos.py
+++ proyecto/codigo/Lectura Datos.py
@@ -16,14 +16,25 @@ class Lectura:
     def main(self):
         size = -1
         with open('datos4.csv', encoding='utf-8', mode='r') as file:
-            read_csv = csv.reader(file, delimiter=';')
-            for row in read_csv:
+            read_csv_size = csv.reader(file, delimiter=';')
+            for row in read_csv_size:
                 if (size == -1):
                     self.column = (row)
                     size += 1
                 else:
-                    self.data.append(row)
                     size += 1
+        file.close()
+        with open('datos4.csv', encoding='utf-8', mode='r') as file:
+            read_csv = csv.reader(file, delimiter=';')
+            self.data=[0]*size
+            first_line = True;
+            i = 0
+            for row in read_csv:
+                if (first_line):
+                    first_line = False
+                else:
+                    self.data[i] = row
+                    i = i+1
         file.close()
         for row in range(len(self.data)):
             self.students.append(self.data [row][0])
@@ -45,6 +56,3 @@ class Lectura:
 
     def add_student(self,student):
         self.data.append(student)
-
-
-
