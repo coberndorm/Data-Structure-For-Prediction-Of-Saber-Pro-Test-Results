3761ccbd97d41f5a0fc570d41a702719edc5a2dc
Mooskixs <elguismo123@gmail.com>
2020-05-16 17:21:00 -0500

    Fixed dataset inputs into ranked, cualitative, numeric and boolean. Added success attribute to student class

--- codigo/arbol.py
+++ codigo/arbol.py
@@ -1,7 +1,11 @@
 from .lectura_datos import get_all_students
 
 class Node:
-    pass
+    def __init__(self, feature, target):
+        self. right = self.left = None
+        self.feature = feature
+        self.target = target
+        
 
 class Arbol:
     pass
---
+++ codigo/gini_impurity.py
@@ -0,0 +1,16 @@
+
+def separacion_datos(students, list_students, column):
+    possibles = []
+    for id in list_students:
+        if isinstance(students[id].data[column], int):
+
+            pass
+        else:
+            possibles.append(students[id].data[column])
+
+
+def separador():
+    pass
+
+def gini_impurity(charecteristic,):
+    pass
