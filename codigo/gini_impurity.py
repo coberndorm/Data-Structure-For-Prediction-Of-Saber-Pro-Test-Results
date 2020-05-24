7c7aef29be14c1339640cf4f3b5162c7dfca9c63
Mooskixs <elguismo123@gmail.com>
2020-05-23 20:42:57 -0500

    Fixed dataset inputs into ranked, cualitative, numeric and boolean. Added success attribute to student class

--- codigo/gini_impurity.py
+++ codigo/gini_impurity.py
@@ -1,16 +1,68 @@
-
 def separacion_datos(students, list_students, column):
     possibles = []
     for id in list_students:
         if isinstance(students[id].data[column], int):
+            # used for ranked data
+            gini_impurity_ranked(students, list_students, column)
+            break
+        elif isinstance(students[id].data[column], float):
+            # used for numeric data
+            gini_impurity_numeric(students, list_students, column)
+            break
+        elif isinstance(students[id].data[column], bool):
+            # used for boolean data
+            break
+        elif isinstance(students[id].data[column], str):
+            # used for cualitative
+            break
 
+
+def gini_impurity_ranked(students, list_students, column):
+    possibles = []
+    for id in list_students:
+        value = students[id].data[column]
+        if value not in possibles:
+            possibles.append(value)
+    possibles.sort()
+    best_gini = -1
+
+    for rank in possibles:
+        for id in list_students:
             pass
-        else:
-            possibles.append(students[id].data[column])
 
 
-def separador():
+def gini_impurity_numeric(students, list_students, column):
+    possibles = []
+    for id in list_students:
+        value = students[id].data[column]
+        if value not in possibles:
+            possibles.append(value)
+    possibles.sort()
+    best_gini = -1
+
+    for rank in possibles:
+        for id in list_students:
+            pass
+
+
+def gini_impurity_ranked(students, list_students, column):
+    possibles = []
+    for id in list_students:
+        value = students[id].data[column]
+        if value not in possibles:
+            possibles.append(value)
+    possibles.sort()
+    best_gini = -1
+
+    for rank in possibles:
+
+        for id in list_students:
+            pass
+
+
+def gini_impurity(charecteristic):
     pass
 
-def gini_impurity(charecteristic,):
+
+if __name__ == '__main__':
     pass
